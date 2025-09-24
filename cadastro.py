c7789cb9eb1563adc521a4919cd6f071dcaf5870
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-09-24 09:46:05 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,10 @@
+print ("cadastro usuario")
+
+nome=input ("Digite seu nome: ")
+cpf= input ("Digite seu cpf: ")
+email=("Digite seu email: ")
+endereco=("Digite seu endereco: ")
+telefone("Digite seu telefone: ")
+
+print(" cadastro finalizado")
+print("\n Nome")
