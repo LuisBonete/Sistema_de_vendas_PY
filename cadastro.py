74f766a0a3f402e0415c5f2c6ff5130e855fda4a
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-09-25 10:37:41 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,10 +1,34 @@
-print ("cadastro usuario")
+import secrets
 
-nome=input ("Digite seu nome: ")
-cpf= input ("Digite seu cpf: ")
-email=("Digite seu email: ")
-endereco=("Digite seu endereco: ")
-telefone("Digite seu telefone: ")
+print("Cadastro de usuário")
 
-print(" cadastro finalizado")
-print("\n Nome")
+nome = input("Digite seu nome: ")
+cpf = input("Digite seu cpf: ")
+email = input("Digite seu email: ")
+endereco = input("Digite seu endereco: ")
+telefone = input("Digite seu telefone: ")
+
+
+
+def gerar_id(digitos=10):
+
+ 
+    limite_minimo = 10**(digitos - 1)
+    limite_maximo = (10**digitos) - 1
+
+    novo_id = secrets.randbelow(limite_maximo - limite_minimo + 1) + limite_minimo
+   
+ 
+    return novo_id
+
+
+id_novo = gerar_id()
+
+print("\nCadastro finalizado")
+
+print(f"Nome: {nome}")
+print(f"CPF: {cpf}")
+print(f"Email: {email}")
+print(f"Endereco: {endereco}")
+print(f"Telefone: {telefone}")
+print(f"Seu ID é: {id_novo}")
