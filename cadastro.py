0ac6070257b0ef825d04516f54063f42aca3fc7f
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-09-26 12:15:11 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -8,27 +8,34 @@ email = input("Digite seu email: ")
 endereco = input("Digite seu endereco: ")
 telefone = input("Digite seu telefone: ")
 
+nome_ot = nome.replace('_',' ').strip()
+
+cpf_ot = cpf.replace('-','').replace('.', '')
+
+email_ot = email.strip()
+
+endereco_ot = endereco.replace('-', '').strip()
+
+telefone_ot = telefone.replace('-', '').replace('(', '').replace(')', '').replace(' ', '').strip()
 
 
 def gerar_id(digitos=10):
-
- 
+    
     limite_minimo = 10**(digitos - 1)
     limite_maximo = (10**digitos) - 1
 
     novo_id = secrets.randbelow(limite_maximo - limite_minimo + 1) + limite_minimo
-   
- 
+    
     return novo_id
-
 
 id_novo = gerar_id()
 
 print("\nCadastro finalizado")
 
-print(f"Nome: {nome}")
-print(f"CPF: {cpf}")
-print(f"Email: {email}")
-print(f"Endereco: {endereco}")
-print(f"Telefone: {telefone}")
-print(f"Seu ID é: {id_novo}")
+
+print(f"Nome: {nome_ot}")
+print(f"CPF: {cpf_ot}")
+print(f"Email: {email_ot}")
+print(f"Endereco: {endereco_ot}")
+print(f"Telefone: {telefone_ot}")
+print(f"Seu ID é: {id_novo}") # Usando 'id_novo' que foi definida
