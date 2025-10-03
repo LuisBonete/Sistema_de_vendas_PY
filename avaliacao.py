759991d0643784090c27e75ba8090e81afa7a993
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-10-02 22:28:41 -0300

    Create avaliacao.py

---
+++ avaliacao.py
@@ -0,0 +1,10 @@
+print("----ADICIONAR AVALIACAO----")
+
+avaliacao = input("Digite de 0 a 10 o quao satisfeito voce esta: ")
+descricao= input("Fale um pouco mais sobre sua avaliacao: 
+    ")
+
+print("\nsua avaliacao foi de", avaliacao)
+print("\nsua descricao foi de:", descricao)
+
+print("\nOBRIGADO PELA SUA COMPREENCAO :) ")
